571d348866c01bd5d6e872922245527fd53eeb3e
Keir Bailey <keirinvestment@gmail.com>
2019-10-02 11:33:18 +0100

    Added ticker_scrape index url dictionary for wikipedia

---
+++ main.py
@@ -0,0 +1,8 @@
+import ticker_scrape
+
+
+# FOR TICKER SCRAPE: Index List Info Dictionary (key = index ticker) (values = [index_urls_dict, ticker_col in table])
+index_urls_dict = {'SPX': ['https://en.wikipedia.org/wiki/List_of_S%26P_500_companies', 0],
+                   'DJI': ['https://en.wikipedia.org/wiki/Dow_Jones_Industrial_Average', 2],
+                   'NDAQ': ['https://en.wikipedia.org/wiki/NASDAQ-100#Components', 1]}
+
